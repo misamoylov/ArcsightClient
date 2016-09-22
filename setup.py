ceffa201fef454b18a79f038b8bcae67c41f8567
misamoylov <mikhailsamoiloff@gmail.com>
2016-09-22 19:37:54 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='ArcsightClient',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/misamoylov/ArcsightClient',
+    license='GPL',
+    author='Mikhail Samoylov',
+    author_email='mikhailsamoiloff@gmail.com',
+    description='ArcsightClient',
+)
